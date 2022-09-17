057bcb0b89564f9e6af9e3edf1801a0322bb72f9
Pesar <pesar369@gmail.com>
2022-09-17 20:41:10 +0430

    updated PWA config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service")
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    name: "Pomodoro",
+    themeColor: "#fff",
+  },
 })
