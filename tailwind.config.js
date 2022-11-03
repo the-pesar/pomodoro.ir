ef08b295047395d61080562a0f77d41612eaa728
Saman-Safaei <xsamansafaeix@gmail.com>
2022-11-03 22:40:27 +0330

    fix index.html file address

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  purge: ["./public/index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
