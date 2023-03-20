84280f493c09d84b3790918274971a70e0f411db
Pesar <pesar369@gmail.com>
2023-03-21 00:24:59 +0330

    added vercel analytics package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@vite-pwa/nuxt"],
   css: ["~/assets/styles/main.css"],
+  plugins: [{ src: "~/plugins/vercel.ts", mode: "client" }],
   pwa: {
     manifest: {
       name: "pomodoro",
