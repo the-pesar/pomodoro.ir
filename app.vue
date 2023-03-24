e6c7a1764557a4ae9a33ede6feb5ae9c9d4b1632
khashayarghajar <khashayarghajar7@gmail.com>
2023-03-24 01:54:15 -0700

    add a ~/layouts/default.vue

--- app.vue
+++ app.vue
@@ -1,4 +1,5 @@
 <template>
-  <VitePwaManifest />
-  <NuxtPage />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,28 +2,4 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@vite-pwa/nuxt"],
   css: ["~/assets/styles/main.css"],
   plugins: [{ src: "~/plugins/vercel.ts", mode: "client" }],
-  pwa: {
-    manifest: {
-      name: "pomodoro",
-      short_name: "pmdr",
-      description: "desc...",
-      icons: [
-        {
-          src: "pwa-192x192.png",
-          sizes: "192x192",
-          type: "image/png",
-        },
-        {
-          src: "pwa-512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-        },
-      ],
-    },
-    client: {
-      installPrompt: true,
-    },
-    workbox: { navigateFallback: "/" },
-    devOptions: { enabled: true, type: "module" },
-  },
 })
