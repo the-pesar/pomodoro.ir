925434afac880c9544ea860cdca6d7b1963e47e4
Pesar <pesar369@gmail.com>
2023-09-08 01:04:30 +0330

    feat: added navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup></script>
+
+<template>
+  <nav class="flex justify-between p-6">
+    <div class="flex">
+      <div class="flex bg-glass p-3 rounded-lg">
+        <img src="@/assets/icons/person.svg" alt="person" />
+        <span class="text-white mx-2">پروفایل</span>
+      </div>
+      <div class="flex bg-glass p-3 rounded-lg mr-3">
+        <img src="@/assets/icons/activity.svg" alt="pesettingsrson" />
+        <span class="text-white mx-2">تاریخچه</span>
+      </div>
+      <div class="flex bg-glass p-3 rounded-lg mr-3">
+        <img src="@/assets/icons/cloud.svg" alt="pesettingsrson" />
+        <span class="text-white mx-2">لیست کاربران</span>
+      </div>
+    </div>
+    <div class="flex">
+      <div class="flex bg-glass p-3 rounded-lg mr-3">
+        <img src="@/assets/icons/settings.svg" alt="pesettingsrson" />
+      </div>
+    </div>
+  </nav>
+</template>
--- components/pmdr.vue
+++ components/pmdr.vue
@@ -1,5 +1,6 @@
 <template>
   <main class="pmdr w-full min-h-screen">
+    <Navbar />
     <div
       class="mx-auto w-12/12 md:w-10/12 lg:w-7/12 xl:w-6/12 2xl:w-4/12 py-5 px-3 md:px-10">
       <SelectedTask />
