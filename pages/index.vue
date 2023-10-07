9f0ee0fdcb0dff203d1ed60eebb75ad65920f199
Pourya Daneshvar <pourya@daneshvar.me>
2023-10-07 14:08:41 +0330

    Added title and description for SEO improves

--- pages/index.vue
+++ pages/index.vue
@@ -7,8 +7,8 @@
 </template>
 <script setup>
 useHead({
-  title: 'پومودورو',
-  meta: [{ name: 'theme-color' }],
+  title: 'مدیریت زمان به روش پومودورو',
+  meta: [{ name: 'theme-color', description: 'یک برنامه ساده برای تکنیک  پومودورو (Pomodoro) که روی مرورگر دسکتاپ و موبایل کار می کند. این برنامه به شما کمک می کند زمان خود را مدیریت کنید و به شما امکان می دهد بر روی هر کاری مانند مطالعه، نوشتن یا کدنویسی تمرکز کنید.' }],
   link: [{ rel: 'manifest', href: 'https://pomodoro.ir/manifest.json' }],
 })
 </script>
